a3805cff9e4c800f2817cb8e79c6eee0aad48863
Vasko <vbblajev@gmail.com>
2024-01-06 23:50:38 +0200

    to do list update

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -5769,7 +5769,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59233" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51037" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
