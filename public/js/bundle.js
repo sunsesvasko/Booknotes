2a167523b00a20ef8aca56194cc2686fa204be95
Vasko <vbblajev@gmail.com>
2024-01-09 17:14:03 +0200

    to-do list chng

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -5969,7 +5969,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59270" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52951" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
