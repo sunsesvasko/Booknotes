d637be278e67f76cca6137c3156a356224aee998
Vasko <vbblajev@gmail.com>
2024-01-23 16:16:15 +0200

    Responsiveness fixed for Nokia T21 tablet

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -6924,7 +6924,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50561" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64337" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
